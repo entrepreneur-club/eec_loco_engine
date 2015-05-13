4f02e45e9d43182d40bc034132dbba7354e2781b
Jonathan Mueller <j.mueller@apoveda.ch>
2015-05-13 14:01:44 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EecLocoEngine::Application.config.secret_token = '4eed1bf9236ef000b951c3537c549da59089d1c32b55daff4b7c63a6d77b4bb3c2b2305c50cb48e4a4694c99b9d7cb476060ccab7d6fd6b7e657b4d38493880c'
